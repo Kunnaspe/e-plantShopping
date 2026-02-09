5f3605419ebc44c005f9a25230fac810ed51c73f
Kunnaspe <122162227+Kunnaspe@users.noreply.github.com>
2026-02-09 14:46:35 -0500

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,1 +1,21 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectCartItems,
+  selectTotalAmount,
+  selectTotalQuantity,
+  incrementQty,
+  decrementQty,
+  removeFromCart
+} from "./redux/CartSlice";
+import "./App.css";
+
+function Navbar() {
+  const totalQty = useSelector(selectTotalQuantity);
+
+  return (
+    <div className="navbar">
+      <div className="navBrand">Paradise Nursery</div>
+      <
 
